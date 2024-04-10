cacc4de753640838334581cd23e2d7de6b6a7e53
Hezron Bwareng <bwarenghezron@gmail.com>
2024-04-10 12:21:10 +0300

    added cart

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CartProvider } from './context/cart.jsx'
+import { CartProvider } from './context/cart'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
